0b18887348ae1f506293cec16a991afdd6786779
Adisa Barakat <adisabarakatadekemi@gmail.com>
2023-03-31 02:32:00 +0100

    adds backend api

---
+++ api/auth/views.py
@@ -0,0 +1,210 @@
+from flask import request
+from flask_restx import Namespace, Resource, fields
+from werkzeug.security import generate_password_hash, check_password_hash
+from http import HTTPStatus
+from ..models.employees import Employee
+from flask_jwt_extended import get_jwt, jwt_required, get_jwt_identity, create_access_token, create_refresh_token, unset_jwt_cookies, JWTManager
+from datetime import timedelta
+from ..utils import admin_required
+
+
+auth_ns = Namespace('auth', description='Authentication related operations')
+
+signup_model = auth_ns.model(
+    'Signup', {
+        'username': fields.String(required=True, description="A username"),
+        'email': fields.String(required=True, description="An email"),
+        'password': fields.String(required=True, description="A password")
+    }
+)
+
+user_model = auth_ns.model(
+    'Employee', {
+        'id': fields.Integer(),
+        'username': fields.String(required=True, description="A username"),
+        'email': fields.String(required=True, description="An email"),
+        'password_hash': fields.String(required=True, description="A password"),
+        'is_admin': fields.Boolean(description="Those who can create meals")
+    }
+)
+
+login_model = auth_ns.model(
+    'Login', {
+        'username': fields.String(required=True, description="A username"),
+        'password': fields.String(required=True, description="A password")
+    }
+)
+
+@auth_ns.route('/admin/signup')
+class AdminSignUp(Resource):
+    @auth_ns.expect(signup_model)
+    @auth_ns.doc(description='Sign up a new admin')
+    def post(self):
+        """
+            Sign up a new admin
+        """
+
+        data = request.get_json()
+
+        user = Employee.get_by_email(data.get('email'))
+
+        # check if user already exists
+        if user:
+            return {'message': 'User already exists'}, HTTPStatus.BAD_REQUEST
+
+        admin = Employee(
+            username = data.get('username'),
+            email = data.get('email'),
+            password_hash = generate_password_hash(data.get('password')),
+            role = 'admin',
+            is_admin = True
+        )
+
+        admin.save()
+
+        return {
+            'message': 'Admin created successfully'
+        }, HTTPStatus.CREATED
+
+
+@auth_ns.route('/login')
+class Login(Resource):
+    @auth_ns.expect(login_model)
+    @auth_ns.doc(description='Login a user')
+    def post(self):
+        """
+            Login a user
+        """
+
+        data = request.get_json()
+
+        username = data.get('username')
+
+        password = data.get('password')
+
+        # check if username and password are provided
+        if not username and not password:
+            return {
+                'message': 'Username and password are required'
+            }, HTTPStatus.BAD_REQUEST
+
+        user = Employee.get_by_username(username)
+
+        # check if user exists
+        if not user:
+            return {
+                'message': 'User not found'
+            }, HTTPStatus.NOT_FOUND
+
+        # check if password is correct
+        if not check_password_hash(user.password_hash, password):
+            return {
+                'message': 'Wrong password'
+            }, HTTPStatus.UNAUTHORIZED
+
+        access_token = create_access_token(identity=user.id, expires_delta=timedelta(hours=1))
+        refresh_token = create_refresh_token(identity=user.id, expires_delta=timedelta(days=30))
+
+        return {
+            'access_token': access_token,
+            'refresh_token': refresh_token
+        }, HTTPStatus.OK
+    
+
+@auth_ns.route('/refresh')
+class Refresh(Resource):
+    @auth_ns.doc(description='Refresh access token')
+    @jwt_required(refresh=True)
+    def post(self):
+        """
+            Refresh access token
+        """
+
+        current_user = get_jwt_identity()
+
+        access_token = create_access_token(identity=current_user, expires_delta=timedelta(hours=1))
+
+        return {
+            'access_token': access_token
+        }, HTTPStatus.OK
+    
+
+@auth_ns.route('/logout')
+class Logout(Resource):
+    @auth_ns.doc(description='Logout a user')
+    @jwt_required()
+    def post(self):
+        """
+            Logout a user
+        """
+
+        jti = get_jwt()['jti']
+
+        return {
+            'message': 'Successfully logged out'
+        }, HTTPStatus.OK
+    
+
+@auth_ns.route('/employee')
+class AdminAddEmployee(Resource):
+    @auth_ns.expect(signup_model)
+    @auth_ns.doc(description='Admin only can add an employee')
+    @admin_required()
+    def post(self):
+        """
+            Add an employee
+        """
+
+        data = request.get_json()
+
+        user = Employee.get_by_email(data.get('email'))
+
+        # check if user already exists
+        if user:
+            return {'message': 'User already exists'}, HTTPStatus.BAD_REQUEST
+
+        new_employee = Employee(
+            username = data.get('username'),
+            email = data.get('email'),
+            password_hash=generate_password_hash(data.get('password')),
+            is_admin=False
+        )
+
+        new_employee.save()
+
+        return {
+            'message': 'User created successfully'
+        }, HTTPStatus.CREATED
+    
+    # get all employees
+    @auth_ns.marshal_list_with(user_model)
+    @auth_ns.doc(description='Get all employees')
+    @admin_required()
+    def get(self):
+        """
+            Get all employees
+        """
+
+        employees = Employee.get_all()
+
+        return employees, HTTPStatus.OK
+    
+@auth_ns.route('/employee/<int:id>')
+class AdminDeleteEmployee(Resource):
+    @auth_ns.doc(description='Admin can delete an employee')
+    @admin_required()
+    def delete(self, id):
+        """
+            Delete an employee
+        """
+
+        employee = Employee.get_by_id(id)
+
+        if not employee:
+            return {'message': 'Employee not found'}, HTTPStatus.NOT_FOUND
+
+        employee.delete()
+
+        return {'message': 'Employee deleted successfully'}, HTTPStatus.OK
+    
+
---
+++ api/meals/views.py
@@ -0,0 +1,99 @@
+from flask import request
+from flask_restx import Namespace, Resource, fields
+from datetime import datetime
+from ..models.meals import Meal
+from ..models.employees import Employee
+from http import HTTPStatus
+from flask_jwt_extended import jwt_required
+from ..utils import admin_required
+
+
+meal_ns = Namespace('meals', description='Meal related operations')
+
+add_meal_model = meal_ns.model(
+    'Meal', {
+        'name': fields.String(required=True, description="A meal name"),
+        'employee_id': fields.Integer(required=True, description="An employee id"),
+        # 'date': fields.Date(required=True, description="A date")
+    }
+)
+
+meal_model = meal_ns.model(
+    'Meal', {
+        'id': fields.Integer(),
+        'name': fields.String(required=True, description="A meal name"),
+        'employee_id': fields.Integer(required=True, description="An employee id"),
+        'date': fields.Date(description="A date")
+    }
+)
+
+
+@meal_ns.route('/meal')
+class AssignMeal(Resource):
+    @meal_ns.expect(meal_model)
+    @meal_ns.doc(description='Admin creates a meal for an employee')
+    @admin_required()
+    def post(self):
+        """
+            Create a new meal
+        """
+        data = request.get_json()
+
+        # employee = Employee.get_by_id(data.get('employee_id'))
+
+        meal = Meal(
+            name = data.get('name'),
+            employee_id = data.get('employee_id'),
+            date = datetime.date.today()
+        )
+
+        meal.save()
+
+        employee_meal = {}
+        employee_meal['name'] = meal.name
+        employee_meal['date'] = meal.date
+        employee_meal['employee_id'] = meal.employee_id
+
+        return {
+            'meal': employee_meal
+        }, HTTPStatus.CREATED
+
+
+    @meal_ns.marshal_list_with(meal_model)
+    @meal_ns.doc(description='Any authenticated user can get all meals')
+    @jwt_required()
+    def get(self):
+        """
+            Get all meals
+        """
+        meals = Meal.get_all()
+
+        return {
+            'meals': meals
+        }, HTTPStatus.OK
+        
+# to check if the employee has already used his meal
+@meal_ns.route('/meal/<int:employee_id>')
+class CheckMeal(Resource):
+    @meal_ns.doc(description='Any authenticated user can get a meal')
+    @jwt_required()
+    def get(self, employee_id):
+        """
+            Get a meal
+        """
+        meal = Meal.get_by_id(employee_id)
+
+        employee = Employee.get_by_id(employee_id)
+
+        if not meal:
+            return {'message': 'Meal not found'}, HTTPStatus.NOT_FOUND
+
+        if employee.meal_used >= 1:
+            return {'message': 'You have already taken your meal today'}, HTTPStatus.BAD_REQUEST
+        
+        employee.meal_used += 1
+
+        employee.update()
+
+        return {'meal': meal}, HTTPStatus.OK
+
